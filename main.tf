a890b10d427165f2935d173174eb55e94b1bea65
plancoarnan <112851879+plancoarnan@users.noreply.github.com>
2024-07-08 14:51:48 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,99 @@
+variable "region" {
+  description = "The AWS region to deploy to"
+  default     = "ap-southeast-1"
+}
+
+variable "ecr_image_uri" {
+  description = "The URI of the ECR image to use for the Lambda function"
+  type        = string
+  default = "200262187471.dkr.ecr.ap-southeast-1.amazonaws.com/everestappdcebfd55/nextappstackfunctione45a3c19repo:nextappstackfunction-0eed6328b9db-v1"
+}
+
+
 provider "aws" {
-  region = "ap-southeast-2"  # Update with your desired AWS region
+  region = var.region
 }
 
-resource "aws_ecr_repository" "my_repo" {
-  name = "everestwithtemplate"
-  image_tag_mutability = "MUTABLE"
+# Lambda IAM role
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
-resource "null_resource" "sam_build" {
-  depends_on = [aws_ecr_repository.my_repo]
+# Attach the AWSLambdaBasicExecutionRole to the IAM role
+resource "aws_iam_role_policy_attachment" "lambda_basic_exec_role_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
 
-  provisioner "local-exec" {
-    command = <<EOT
-      cd path_to_your_sam_app_directory
-      sam build --template template.yaml --image-repository ${aws_ecr_repository.my_repo.repository_url}
-    EOT
+# Lambda function
+resource "aws_lambda_function" "my_lambda_function" {
+  function_name     = "my_container_lambda"
+  image_uri         = var.ecr_image_uri
+  package_type      = "Image"
+  role              = aws_iam_role.lambda_exec_role.arn
+  timeout           = 30
+
+  lifecycle {
+    ignore_changes = [
+      image_uri,
+    ]
   }
 }
 
-resource "null_resource" "sam_deploy" {
-  depends_on = [null_resource.sam_build]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      cd path_to_your_sam_app_directory
-      sam deploy --template-file .aws-sam/build/template.yaml --stack-name my-sam-stack --capabilities CAPABILITY_IAM --image-repository ${aws_ecr_repository.my_repo.repository_url} --region us-east-1 --no-confirm-changeset
-    EOT
-  }
+# API Gateway HTTP API
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "my_http_api"
+  protocol_type = "HTTP"
 }
+
+# API Gateway Integration
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id             = aws_apigatewayv2_api.http_api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.my_lambda_function.invoke_arn
+  payload_format_version = "2.0"
+}
+
+# API Gateway Route
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+# API Gateway Stage
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+# Lambda Permission for API Gateway
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+output "api_endpoint" {
+  description = "The HTTP API endpoint"
+  value       = aws_apigatewayv2_api.http_api.api_endpoint
+}
+

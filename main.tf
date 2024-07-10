d152b329c5f75dc050de2fd6f66cb0cccc098813
plancoarnan <112851879+plancoarnan@users.noreply.github.com>
2024-07-10 14:41:11 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,99 +1,49 @@
-variable "region" {
-  description = "The AWS region to deploy to"
-  default     = "ap-southeast-1"
-}
-
-variable "ecr_image_uri" {
-  description = "The URI of the ECR image to use for the Lambda function"
+variable "repository_name" {
+  description = "The name of the ECR repository."
   type        = string
-  default = "200262187471.dkr.ecr.ap-southeast-1.amazonaws.com/everestappdcebfd55/nextappstackfunctione45a3c19repo:nextappstackfunction-0eed6328b9db-v1"
+  default = "everestappdcebfd55/nextappstackfunctione45a3c19repo"
 }
 
+variable "new_tag_name" {
+  description = "The new tag name for the Docker image."
+  type        = string
+}
+
+variable "dockerfile_path" {
+  description = "Path to the Dockerfile."
+  type        = string
+  default = "."
+}
+
+variable "image_name" {
+  description = "Name of the Docker image to build."
+  type        = string
+  default = "everestappdcebfd55/nextappstackfunctione45a3c19repo"
+}
 
 provider "aws" {
-  region = var.region
+  region = "ap-southeast-1"  # Change the region as needed
 }
 
-# Lambda IAM role
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "lambda_exec_role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+data "aws_ecr_repository" "my_ecr" {
+  name = var.repository_name
 }
 
-# Attach the AWSLambdaBasicExecutionRole to the IAM role
-resource "aws_iam_role_policy_attachment" "lambda_basic_exec_role_attach" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-# Lambda function
-resource "aws_lambda_function" "my_lambda_function" {
-  function_name     = "my_container_lambda"
-  image_uri         = var.ecr_image_uri
-  package_type      = "Image"
-  role              = aws_iam_role.lambda_exec_role.arn
-  timeout           = 30
-
-  lifecycle {
-    ignore_changes = [
-      image_uri,
-    ]
+resource "null_resource" "docker_build_and_tag" {
+  provisioner "local-exec" {
+    command = <<EOT
+      docker build -t ${var.image_name}:latest ${var.dockerfile_path}
+      docker tag ${var.image_name}:latest ${data.aws_ecr_repository.my_ecr.repository_url}:${var.new_tag_name}
+    EOT
   }
+
+  depends_on = [data.aws_ecr_repository.my_ecr]
 }
 
-# API Gateway HTTP API
-resource "aws_apigatewayv2_api" "http_api" {
-  name          = "my_http_api"
-  protocol_type = "HTTP"
-}
+resource "null_resource" "docker_push" {
+  provisioner "local-exec" {
+    command = "docker push ${data.aws_ecr_repository.my_ecr.repository_url}:${var.new_tag_name}"
+  }
 
-# API Gateway Integration
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id             = aws_apigatewayv2_api.http_api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.my_lambda_function.invoke_arn
-  payload_format_version = "2.0"
+  depends_on = [null_resource.docker_build_and_tag]
 }
-
-# API Gateway Route
-resource "aws_apigatewayv2_route" "default_route" {
-  api_id    = aws_apigatewayv2_api.http_api.id
-  route_key = "$default"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
-
-# API Gateway Stage
-resource "aws_apigatewayv2_stage" "default_stage" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "$default"
-  auto_deploy = true
-}
-
-# Lambda Permission for API Gateway
-resource "aws_lambda_permission" "allow_api_gateway" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.my_lambda_function.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
-}
-
-output "api_endpoint" {
-  description = "The HTTP API endpoint"
-  value       = aws_apigatewayv2_api.http_api.api_endpoint
-}
-
